ff2d50787b653a5e3d5e5dd3d2ac352df163642a
liukefu <liukefu2020@sina.com>
2020-12-17 16:40:55 +0800

    1

--- App.js
+++ App.js
@@ -1,114 +1,345 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
-import React from 'react';
+import React, {Component} from 'react';
 import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
+  View, Text, StyleSheet, ScrollView,
+  Image, TouchableOpacity, NativeModules, Dimensions,
 } from 'react-native';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import Video from 'react-native-video';
 
-const App: () => React$Node = () => {
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
-  );
-};
+import SYImagePicker from 'react-native-syan-image-picker';
+
+
+import ImagePicker from 'react-native-customized-image-picker';
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
-  engine: {
-    position: 'absolute',
-    right: 0,
+  button: {
+    backgroundColor: 'blue',
+    marginBottom: 10,
   },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
+  text: {
+    color: 'white',
+    fontSize: 20,
+    textAlign: 'center',
   },
 });
 
-export default App;
+export default class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      images: null,
+    };
+  }
+
+  pickSingleBase64(cropit) {
+    ImagePicker.openPicker({
+      width: 300,
+      height: 300,
+      cropping: cropit,
+      includeBase64: true,
+    }).then(images => {
+      this.setState({
+        images: images.map(i => {
+          console.log('received image', i);
+          return {uri: i.path, width: i.width, height: i.height, mime: i.mime};
+        }),
+      });
+    }).catch(e => alert(e));
+  }
+
+  pickSingle(cropit) {
+    ImagePicker.openPicker({
+      width: 300,
+      height: 300,
+      cropping: cropit,
+      isSelectBoth: true,
+      isCamera: true,
+    }).then(images => {
+      this.setState({
+        images: images.map(i => {
+          console.log('received image', i);
+          return {uri: i.path, width: i.width, height: i.height, mime: i.mime};
+        }),
+      });
+    }).catch(e => {
+      console.log(e.code);
+      alert(e);
+    });
+  }
+
+  pickSingleVideo(cropit) {
+    ImagePicker.openPicker({
+      width: 300,
+      height: 300,
+      isVideo: true,
+      isCamera: true,
+      cropping: cropit,
+    }).then(images => {
+      this.setState({
+        images: images.map(i => {
+          console.log('received image', i);
+          return {uri: i.path, width: i.width, height: i.height, mime: i.mime};
+        }),
+      });
+    }).catch(e => {
+      console.log(e.code);
+      alert(e);
+    });
+  }
+
+  pickMultipleVideo(cropit) {
+    ImagePicker.openPicker({
+      width: 300,
+      height: 300,
+      isVideo: true,
+      isCamera: true,
+      multiple: true,
+      cropping: cropit,
+    }).then(images => {
+      this.setState({
+        images: images.map(i => {
+          console.log('received image', i);
+          return {uri: i.path, width: i.width, height: i.height, mime: i.mime};
+        }),
+      });
+    }).catch(e => {
+      console.log(e.code);
+      alert(e);
+    });
+  }
+
+  pickSingleAndCamera() {
+    ImagePicker.openPicker({
+      isCamera: true,
+      openCameraOnStart: true,
+      returnAfterShot: true,
+    }).then(images => {
+      this.setState({
+        images: images.map(i => {
+          console.log('received image', i);
+          return {uri: i.path, width: i.width, height: i.height, mime: i.mime};
+        }),
+      });
+    }).catch(e => {
+      console.log(e.code);
+      alert(e);
+    });
+  }
+
+  pickMultiple() {
+    ImagePicker.openPicker({
+      isCamera: true,
+      multiple: true,
+    }).then(images => {
+      this.setState({
+        images: images.map(i => {
+          console.log('received image', i);
+          return {uri: i.path, width: i.width, height: i.height, mime: i.mime};
+        }),
+      });
+    }).catch(e => alert(e));
+  }
+
+  scaledHeight(oldW, oldH, newW) {
+    return (oldH / oldW) * newW;
+  }
+
+  renderVideo(uri) {
+    return <View style={{height: 300, width: 300}}>
+      <Video source={{uri}}
+             style={{
+               position: 'absolute',
+               top: 0,
+               left: 0,
+               bottom: 0,
+               right: 0,
+             }}
+             rate={1}
+             paused={false}
+             volume={1}
+             muted={false}
+             resizeMode={'cover'}
+             onLoad={load => console.log(load)}
+             onProgress={() => {
+             }}
+             onEnd={() => {
+               console.log('Done!');
+             }}
+             repeat={true}/>
+    </View>;
+  }
+
+  renderImage(image) {
+    return <Image style={{width: 300, height: 300, resizeMode: 'contain'}} source={image}/>;
+  }
+
+  renderAsset(image) {
+    if (image.mime && image.mime.toLowerCase().indexOf('video/') !== -1) {
+      return this.renderVideo(image.uri);
+    }
+
+    return this.renderImage(image);
+  }
+
+  render() {
+    return <View style={styles.container}>
+      <ScrollView>
+        {this.state.images ? this.state.images.map(i => <View key={i.uri}>{this.renderAsset(i)}</View>) : null}
+      </ScrollView>
+
+      <TouchableOpacity onPress={() => this.pickSingle(false)} style={styles.button}>
+        <Text style={styles.text}>Select Single</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => this.pickSingleVideo(false)} style={styles.button}>
+        <Text style={styles.text}>Select Single video</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => this.pickMultipleVideo(false)} style={styles.button}>
+        <Text style={styles.text}>Select multiple video</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => this.pickSingleBase64(false)} style={styles.button}>
+        <Text style={styles.text}>Select Single Returning Base64</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => this.pickSingle(true)} style={styles.button}>
+        <Text style={styles.text}>Select Single With Cropping</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => this.pickSingleAndCamera()} style={styles.button}>
+        <Text style={styles.text}>Select Single With Camera</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={this.pickMultiple.bind(this)} style={styles.button}>
+        <Text style={styles.text}>Select Multiple</Text>
+      </TouchableOpacity>
+
+      <View>
+        <TouchableOpacity onPress={this.handleLaunchCamera.bind(this)} style={styles.button}>
+          <Text style={styles.text}>拍照</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={this.handleOpenImagePicker.bind(this)} style={styles.button}>
+          <Text style={styles.text}>开启压缩</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={this.handleAsyncSelectPhoto.bind(this)} style={styles.button}>
+          <Text style={styles.text}>关闭压缩</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={this.handlePromiseSelectPhoto.bind(this)} style={styles.button}>
+          <Text style={styles.text}>选择照片(Promise)带base64</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={this.handleDeleteCache.bind(this)} style={styles.button}>
+          <Text style={styles.text}>缓存清除</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={this.handleOpenVideoPicker.bind(this)} style={styles.button}>
+          <Text style={styles.text}>选择视频</Text>
+        </TouchableOpacity>
+      </View>
+    </View>;
+  }
+
+
+  handleOpenImagePicker = () => {
+    SYImagePicker.showImagePicker(
+        {
+          imageCount: 1,
+          isRecordSelected: true,
+          isCrop: true,
+          showCropCircle: true,
+          quality: 90,
+          compress: true,
+          enableBase64: false,
+        },
+        (err, photos) => {
+          console.log('开启', err, photos);
+          if (!err) {
+            this.setState({
+              photos,
+            });
+          } else {
+            console.log(err);
+          }
+        },
+    );
+  };
+
+  /**
+   * 使用方式sync/await
+   * 相册参数暂时只支持默认参数中罗列的属性；
+   * @returns {Promise<void>}
+   */
+  handleAsyncSelectPhoto = async () => {
+    // SYImagePicker.removeAllPhoto()
+    try {
+      const photos = await SYImagePicker.asyncShowImagePicker({
+        // allowPickingOriginalPhoto: true,
+        imageCount: 1,
+        isGif: true,
+        enableBase64: true,
+      });
+      console.log('关闭', photos);
+      // 选择成功
+      this.setState({
+        photos: [...this.state.photos, ...photos],
+      });
+    } catch (err) {
+      console.log(err);
+      // 取消选择，err.message为"取消"
+    }
+  };
+
+  handlePromiseSelectPhoto = () => {
+    SYImagePicker.asyncShowImagePicker({imageCount: 3})
+        .then(photos => {
+          console.log(photos);
+          const arr = photos.map(v => {
+            return v;
+          });
+          // 选择成功
+          this.setState({
+            photos: [...this.state.photos, ...arr],
+          });
+        })
+        .catch(err => {
+          // 取消选择，err.message为"取消"
+        });
+  };
+
+  handleLaunchCamera = async () => {
+    await this.requestPermission();
+    SYImagePicker.openCamera(
+        {isCrop: true, showCropCircle: true, showCropFrame: false},
+        (err, photos) => {
+          console.log(err, photos);
+          if (!err) {
+            this.setState({
+              photos: [...this.state.photos, ...photos],
+            });
+          }
+        },
+    );
+  };
+
+  handleDeleteCache = () => {
+    SYImagePicker.deleteCache();
+  };
+
+  handleOpenVideoPicker = () => {
+    SYImagePicker.openVideoPicker(
+        {allowPickingMultipleVideo: true},
+        (err, res) => {
+          console.log(err, res);
+          if (!err) {
+            let photos = [...this.state.photos];
+            res.map(v => {
+              photos.push({...v, uri: v.coverUri});
+            });
+            this.setState({
+              photos,
+            });
+          }
+        },
+    );
+  };
+
+}
